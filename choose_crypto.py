29bbd75b64fb1616161631239b690fa287f5fe73
petya0927 <unyipeti@gmail.com>
2021-08-03 15:20:58 +0200

    Can choose crypto without Plot! button

--- choose_crypto.py
+++ choose_crypto.py
@@ -13,15 +13,23 @@ CURRENCIES = [
     'EUR',
     'HUF']
 
+coin_choosed, currency_choosed = False, False
+
 def change_coin(value):
     global COIN
+    global coin_choosed
     COIN = value
-    return COIN
+    coin_choosed = True
+    print(COIN, coin_choosed)
+    return COIN, coin_choosed
 
 def change_curr(value):
     global CURRENCY
+    global currency_choosed
     CURRENCY = value
-    return CURRENCY
+    currency_choosed = True
+    print(CURRENCY, currency_choosed)
+    return CURRENCY, currency_choosed
 
 def choose_coin(root):
 
@@ -30,17 +38,17 @@ def choose_coin(root):
     canvas = tkinter.Canvas(root)
     canvas.grid()
 
-    for coin_opt in COINS:
-        btn = ttk.Button(canvas, text=coin_opt, command=None)
-        btn.grid()
-        btn['command'] = lambda btn=btn: change_coin(btn['text'])
+    for coin_opt in range(len(COINS)):
+        btn = ttk.Button(canvas, text=COINS[coin_opt], command=None)
+        btn.grid(row=coin_opt, column=0)
+        btn['command'] =  lambda btn=btn: [change_coin(btn['text']), crypto.main(root, COIN, CURRENCY) if currency_choosed and coin_choosed else None]
 
-    for currency_opt in CURRENCIES:
-        btn = ttk.Button(canvas, text=currency_opt, command=None)
-        btn.grid()
-        btn['command'] = lambda btn=btn: change_curr(btn['text'])
+    for currency_opt in range(len(CURRENCIES)):
+        btn = ttk.Button(canvas, text=CURRENCIES[currency_opt], command=None)
+        btn.grid(row=currency_opt, column=1)
+        btn['command'] = lambda btn=btn: [change_curr(btn['text']), crypto.main(root, COIN, CURRENCY) if currency_choosed and coin_choosed else None]
 
-    tkinter.Button(root, text='Plot!', command=lambda:crypto.main(root, COIN, CURRENCY)).grid()
+    #tkinter.Button(root, text='Plot!', command=lambda: crypto.main(root, COIN, CURRENCY)).grid()
 
 def main(root):
     choose_coin(root)
--- crypto.py
+++ crypto.py
@@ -1,6 +1,5 @@
 import cryptocompare
 from datetime import datetime
-from cryptocompare.cryptocompare import CURRENCY
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter
@@ -8,11 +7,23 @@ from tkinter import Canvas, ttk
 import time
 
 def config_plot(ax, coin, currency, current_price, price_lst, time_lst):
-    ax.set_title(f'{coin}/{currency}\nPrice: {current_price} USD\nChange: {round(change_percent(price_lst[-2], price_lst[-1]), 2)}%\n24h change: {round(change_percent(price_lst[0], price_lst[-1]), 2)}%' + str(time.strftime('%H:%M')))
+    CURRENCIES = {
+        'USD': '$',
+        'EUR': '€',
+        'HUF': 'Ft'
+    }
+
+    ax.set_title(f'{coin}/{currency}\nPrice: {current_price} {CURRENCIES[currency]}\nChange: {round(change_percent(price_lst[-2], price_lst[-1]), 2)}%\n24h change: {round(change_percent(price_lst[0], price_lst[-1]), 2)}%')
     ax.set_xticks(ticks=[0, len(price_lst)// 2, len(price_lst)]) 
     ax.set_xticklabels([time_lst[0], time_lst[len(price_lst) // 2], time_lst[-1]])
     ax.set_xlabel('Time [day]')
     ax.set_ylabel(f'Price [{currency}]')
+
+    ax.text(len(time_lst), price_lst[-1], str(current_price) + CURRENCIES[currency])
+    max_price = max(price_lst)
+    min_price = min(price_lst)
+    ax.text(price_lst.index(max_price), price_lst[price_lst.index(max_price)], str(max_price) + CURRENCIES[currency])
+    ax.text(price_lst.index(min_price), price_lst[price_lst.index(min_price)], str(min_price) + CURRENCIES[currency])
 
 def get_history(coin, currency):
     prices, times = [], []
@@ -34,14 +45,7 @@ def plot_prices(ax, price_lst):
 
 def change_percent(prev, curr):
     return 100*(curr-prev)/prev
-
-def price_text(ax, price_lst, time_lst, current_price):
-    ax.text(len(time_lst), price_lst[-1], str(current_price) + '$')
-    max_price = max(price_lst)
-    min_price = min(price_lst)
-    ax.text(price_lst.index(max_price), price_lst[price_lst.index(max_price)], str(max_price) + '$')
-    ax.text(price_lst.index(min_price), price_lst[price_lst.index(min_price)], str(min_price) + '$')
-
+    
 def plot_ui(root, fig):
     from main_menu import Main_menu
 
@@ -59,25 +63,23 @@ def plot_ui(root, fig):
     mainmenu_button = tkinter.Button(root, text='Main menu', command= lambda: Main_menu.menu_ui(root))
     mainmenu_button.grid()
 
-def plotting(fig, ax, coin, currency):
+def plotting(root, fig, ax, coin, currency):
 
     current_price = cryptocompare.get_price(coin, currency)[coin][currency]
     prices, times = get_history(coin, currency)
 
     ax.cla()
     config_plot(ax, coin, currency, current_price, prices, times)
-    price_text(ax, prices, times, current_price)
     plot_prices(ax, prices)
     fig.canvas.draw()
 
-    root.after(60000, lambda: plotting(fig, ax, coin, currency))
+    root.after(60000, lambda: plotting(root, fig, ax, coin, currency))
 
 def main(root, coin, currency):
-    print(coin, currency)
     fig, ax = plt.subplots()
     plot_ui(root, fig)
 
-    plotting(fig, ax, coin, currency)
+    plotting(root, fig, ax, coin, currency)
 
     root.mainloop()
 
